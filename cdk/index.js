61681bb959302562d4a7c92812b72e9af01118a0
Oren <orengolan@gmail.com>
2018-11-08 13:54:32 -0800

    misc

--- cdk/index.js
+++ cdk/index.js
@@ -1,24 +1,9 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
+Object.defineProperty(exports, "__esModule", { value: true });
+const cdk = require("@aws-cdk/cdk");
+class MyApp extends cdk.App {
+    constructor() {
+        super();
     }
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-exports.__esModule = true;
-var cdk = require("@aws-cdk/cdk");
-var MyApp = /** @class */ (function (_super) {
-    __extends(MyApp, _super);
-    function MyApp(argv) {
-        return _super.call(this, argv) || this;
-    }
-    return MyApp;
-}(cdk.App));
+}
 new MyApp().run();
--- cdk/index.ts
+++ cdk/index.ts
@@ -1,8 +1,8 @@
 import cdk = require('@aws-cdk/cdk');
 
 class MyApp extends cdk.App {
-    constructor(argv: string[]) {
-        super(argv);
+    constructor() {
+        super();
     }
 }
 
